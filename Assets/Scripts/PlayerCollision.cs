6f4361515b368aae66169f79b3d230a7b2bce532
huzefa <huzefa@umich.edu>
2018-09-28 23:17:30 -0400

    added score

---
+++ Assets/Scripts/PlayerCollision.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour {
+
+    public PlayerMovement Movement;
+
+	void OnCollisionEnter(Collision collisioninfo)
+    {
+        if (collisioninfo.collider.tag == "Obstacle"){
+            Movement.enabled = false;
+        }
+    }
+}
